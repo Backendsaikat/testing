bcc48950406cd53f6ae4dc5d418533c83654eb6d
Saikat Adhikary <93902688+saikatpythondev@users.noreply.github.com>
2025-04-12 23:17:55 +0530

    change4s

--- connect.js
+++ connect.js
@@ -2,10 +2,7 @@ const mongoose = require('mongoose');
 const env = require('dotenv').config(); // Load environment variables from .env file
 // MongoDB URL
 const MONGODB_URI=process.env.MONGODB_URI; // Use the URL from the environment variable
-mongoose.connect(MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+mongoose.connect(MONGODB_URI, {})
 .then(() => {
     console.log("Connection successful");
 })
